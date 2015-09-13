b507227ee53a116b6ea6555fc015ac7f57365610
damedebugger <anja.kefala@gmail.com>
2015-09-13 10:25:07 -0400

    Add answer for ex1

--- exercises/ex1_hello_world_answer.py
+++ exercises/ex1_hello_world_answer.py
@@ -1,1 +1,6 @@
-__author__ = 'Anja'
+# Assign the raw_input value to a variable
+# Your program waits for the user to hit enter
+name = raw_input("Please enter your name: ")
+
+# Then continue to this line
+print("Hello " + name)
