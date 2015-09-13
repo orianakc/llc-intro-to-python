db568c7dfa4c9cb0ccc46027a8ef8a6e31a58b1d
damedebugger <anja.kefala@gmail.com>
2015-09-13 12:17:53 -0400

    Add solution to exercise 4

---
+++ exercises/ex4_chapter_lead_finder_answer.py
@@ -0,0 +1,20 @@
+# Import the csv library
+import csv
+
+# Open the 'llc-chapters.csv' file
+chapters = open('llc-chapters.csv', mode='r')
+
+# Convert it to a csv_data structure
+
+chapters_data = csv.DictReader(chapters)
+
+# Loop through each of the rows
+for chapter in chapters_data:
+    # Compare the 'City' in the row with your city
+    if chapter['City'] == 'Montreal':
+        # Print "Thank you [[Chapter Lead(s)]]!"
+        print('Thank you, ' + chapter['Chapter Lead(s)'] + '!!!')
+
+
+
+
