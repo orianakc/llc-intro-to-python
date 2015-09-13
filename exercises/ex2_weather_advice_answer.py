39edf3023eb72844a998a0e78e60b34bd02251b3
damedebugger <anja.kefala@gmail.com>
2015-09-13 10:29:11 -0400

    Add ex2 solution

---
+++ exercises/ex2_weather_advice_answer.py
@@ -0,0 +1,9 @@
+# file: ex2_weather_advice.py
+weather = raw_input("What is the weather? (cold, raining, etc.)")
+
+if weather == "cold":
+    print("Wear a sweater!")
+elif weather == "raining":
+    print("Bring an umbrella")
+else:
+    print("Dress normally : )")
